2300089feb9170283a3604d0b52724bf5e11c89d
zylka44 <zancza@gmail.com>
2020-05-02 09:48:46 +0200

    Updates

---
+++ precache-manifest.4c8f122f2c658a7c1d75f31e2c6d6c3c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25aecbbcc07a369c971fd68228f4f6e7",
+    "url": "/myhomemenu/index.html"
+  },
+  {
+    "revision": "340bacf27a9f813ec66c",
+    "url": "/myhomemenu/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f138f6e5b311f9dde77a",
+    "url": "/myhomemenu/static/css/main.979a586b.chunk.css"
+  },
+  {
+    "revision": "340bacf27a9f813ec66c",
+    "url": "/myhomemenu/static/js/2.0da06de2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myhomemenu/static/js/2.0da06de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f138f6e5b311f9dde77a",
+    "url": "/myhomemenu/static/js/main.3ad53143.chunk.js"
+  },
+  {
+    "revision": "34742c253107d4e51208",
+    "url": "/myhomemenu/static/js/runtime-main.b46ba531.js"
+  },
+  {
+    "revision": "e375bf9dbc166c10e3ca369948230143",
+    "url": "/myhomemenu/static/media/banner.e375bf9d.png"
+  },
+  {
+    "revision": "ec9967abc5b7fb610520b1602b87ee7f",
+    "url": "/myhomemenu/static/media/broth.ec9967ab.jpg"
+  },
+  {
+    "revision": "30e4c912c960668772ea917d86a3ff71",
+    "url": "/myhomemenu/static/media/fruitsoup.30e4c912.jpg"
+  },
+  {
+    "revision": "270d9a7ab44f93c86e3a894f1c9ef6fd",
+    "url": "/myhomemenu/static/media/khachapuri.270d9a7a.jpg"
+  },
+  {
+    "revision": "7da0bf06a35579f29716a5a7f318ff9a",
+    "url": "/myhomemenu/static/media/kopytka.7da0bf06.jpg"
+  },
+  {
+    "revision": "ea98aef878e91a5a689c5c2b8d51cbb4",
+    "url": "/myhomemenu/static/media/pizza.ea98aef8.jpg"
+  },
+  {
+    "revision": "549429a58cad173eb81907bf6883d9c2",
+    "url": "/myhomemenu/static/media/porridge.549429a5.jpg"
+  },
+  {
+    "revision": "e0573021619a5636975cbe04ef637d37",
+    "url": "/myhomemenu/static/media/ratatouille.e0573021.jpg"
+  },
+  {
+    "revision": "8eb8395b60554546e9a75d6e9cd21c53",
+    "url": "/myhomemenu/static/media/shakshuka.8eb8395b.jpg"
+  },
+  {
+    "revision": "d8e3d59815b53bf12c830e0403b9d539",
+    "url": "/myhomemenu/static/media/spaghetti.d8e3d598.jpg"
+  },
+  {
+    "revision": "5783d63db6b550ccc71a97717d6e037f",
+    "url": "/myhomemenu/static/media/sushi.5783d63d.jpg"
+  },
+  {
+    "revision": "fc2dc9d937bb680d3b183a683fe3a2cf",
+    "url": "/myhomemenu/static/media/tomatosoup.fc2dc9d9.jpg"
+  }
+]);
